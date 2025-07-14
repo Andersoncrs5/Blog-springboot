ee5a045bdc1440c5a5f8b7f68a21d87aa7ca4630
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-14 16:26:06 -0300

    Added UserPreferenceRepository

---
+++ src/main/java/br/com/Blog/api/repositories/UserPreferenceRepository.java
@@ -0,0 +1,9 @@
+package br.com.Blog.api.repositories;
+
+import br.com.Blog.api.entities.UserPreference;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserPreferenceRepository extends JpaRepository<UserPreference, Long> {
+}
