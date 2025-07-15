22fb572dc512433c512f6a53d787301cbd8db938
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-15 14:57:17 -0300

    Added method findByUser

--- src/main/java/br/com/Blog/api/repositories/UserConfigRepository.java
+++ src/main/java/br/com/Blog/api/repositories/UserConfigRepository.java
@@ -1,7 +1,11 @@
 package br.com.Blog.api.repositories;
 
+import br.com.Blog.api.entities.User;
 import br.com.Blog.api.entities.UserConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserConfigRepository extends JpaRepository<UserConfig, Long> {
+    Optional<UserConfig> findByUser(User user);
 }
