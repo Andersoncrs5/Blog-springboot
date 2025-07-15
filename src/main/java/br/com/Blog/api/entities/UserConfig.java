261bdc0e8c4c74caabe59b7e1da5f958bca392a5
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-15 13:44:25 -0300

    Fix error

--- src/main/java/br/com/Blog/api/entities/UserConfig.java
+++ src/main/java/br/com/Blog/api/entities/UserConfig.java
@@ -12,7 +12,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "users")
+@Table(name = "user_config")
 @EntityListeners(AuditingEntityListener.class)
 @Setter
 @Getter
