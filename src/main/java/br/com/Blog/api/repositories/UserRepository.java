c0db3f6cf7edd9bf27211ed9707d9b5ecfb69607
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-16 14:50:07 -0300

    new method findAll

--- src/main/java/br/com/Blog/api/repositories/UserRepository.java
+++ src/main/java/br/com/Blog/api/repositories/UserRepository.java
@@ -2,7 +2,7 @@ package br.com.Blog.api.repositories;
 
 import br.com.Blog.api.entities.User;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Pageable;import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -13,4 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByName(String superadmin);
 
     Page<User> findAllByRoles_Name(String adminRoleName, Pageable pageable);
+
+    Page<User> findAll(Pageable pageable, Specification<User> specs);
 }
