f1c2678049bfd1ce9af30ddfd42432d79107bd21
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-20 15:31:51 -0300

    new method findAllByPost

--- src/main/java/br/com/Blog/api/repositories/MediaRepository.java
+++ src/main/java/br/com/Blog/api/repositories/MediaRepository.java
@@ -1,7 +1,11 @@
 package br.com.Blog.api.repositories;
 
 import br.com.Blog.api.entities.Media;
+import br.com.Blog.api.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MediaRepository extends JpaRepository<Media, Long> {
+    List<Media> findAllByPost(Post post);
 }
