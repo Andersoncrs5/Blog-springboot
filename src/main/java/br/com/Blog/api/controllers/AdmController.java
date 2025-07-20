95a542208c3016df5b565b623407c0f9a781bff3
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-20 15:28:26 -0300

    Methods and endpoint to adm handle buckets

--- src/main/java/br/com/Blog/api/controllers/AdmController.java
+++ src/main/java/br/com/Blog/api/controllers/AdmController.java
@@ -1,6 +1,6 @@
 package br.com.Blog.api.controllers;
 
-import br.com.Blog.api.DTOs.BlockTimeDTO;
+import br.com.Blog.api.DTOs.*;
 import br.com.Blog.api.config.annotation.RateLimit;
 import br.com.Blog.api.controllers.pagebleDtos.PageableDTO;
 import br.com.Blog.api.controllers.setUnitOfWork.UnitOfWork;
@@ -19,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -27,6 +28,166 @@ public class AdmController {
 
     @Autowired
     private UnitOfWork unit;
+
+    @PutMapping("enable-versioning-bucket")
+    @RateLimit(capacity = 15, refillTokens = 5, refillSeconds = 10)
+    @SecurityRequirement(name = "bearerAuth")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    public ResponseEntity<?> enableBucketVersioning(@RequestBody BucketDTO dto, HttpServletRequest request) {
+        this.unit.s3Service.createBucket(dto.bucket());
+        this.unit.s3Service.enableBucketVersioning(dto.bucket());
+
+        Map<String, Object> response = this.unit.responseDefault.response(
+                "Bucket enabled versioning!",
+                200,
+                request.getRequestURL().toString(),
+                "",
+                true
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PutMapping("suspended-versioning-bucket")
+    @RateLimit(capacity = 15, refillTokens = 5, refillSeconds = 10)
+    @SecurityRequirement(name = "bearerAuth")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    public ResponseEntity<?> suspendedBucketVersioning(@RequestBody BucketDTO dto, HttpServletRequest request) {
+        this.unit.s3Service.createBucket(dto.bucket());
+        this.unit.s3Service.suspendedBucketVersioning(dto.bucket());
+
+        Map<String, Object> response = this.unit.responseDefault.response(
+                "Bucket suspended versioning!",
+                200,
+                request.getRequestURL().toString(),
+                "",
+                true
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/create-bucket")
+    @RateLimit(capacity = 15, refillTokens = 5, refillSeconds = 10)
+    @SecurityRequirement(name = "bearerAuth")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    public ResponseEntity<?> createBucket(@RequestBody BucketDTO dto, HttpServletRequest request) {
+        this.unit.s3Service.createBucket(dto.bucket());
+
+        Map<String, Object> response = this.unit.responseDefault.response(
+                "Bucket created",
+                200,
+                request.getRequestURL().toString(),
+                "",
+                true
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete-bucket")
+    @RateLimit(capacity = 15, refillTokens = 5, refillSeconds = 10)
+    @SecurityRequirement(name = "bearerAuth")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    public ResponseEntity<?> deleteBucket(@RequestBody BucketDTO dto, HttpServletRequest request) {
+        this.unit.s3Service.bucketExists(dto.bucket());
+        this.unit.s3Service.deleteBucket(dto.bucket());
+
+        Map<String, Object> response = this.unit.responseDefault.response(
+                "Bucket deleted",
+                200,
+                request.getRequestURL().toString(),
+                "",
+                true
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("get-all-bucket")
+    @RateLimit(capacity = 15, refillTokens = 5, refillSeconds = 8)
+    @SecurityRequirement(name = "bearerAuth")
+    public ResponseEntity<?> getAllBuckets(HttpServletRequest request) {
+        List<String> buckets = this.unit.s3Service.listAllBuckets();
+
+        return ResponseEntity.ok(buckets);
+    }
+
+    @DeleteMapping("/delete-new-object")
+    @RateLimit(capacity = 15, refillTokens = 5, refillSeconds = 10)
+    @SecurityRequirement(name = "bearerAuth")
+    @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
+    public ResponseEntity<?> deleteObject(@RequestBody ObjectDTO dto, HttpServletRequest request) {
+        this.unit.s3Service.bucketExists(dto.bucketName());
+        this.unit.s3Service.deleteObject(dto.bucketName(), dto.key());
+
+        Map<String, Object> response = this.unit.responseDefault.response(
+                "Object deleted",
+                200,
+                request.getRequestURL().toString(),
+                "",
+                true
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PutMapping("/apply-life-cycle-policy-abort-incomplete-multipart-uploads")
+    @RateLimit(capacity = 15, refillTokens = 5, refillSeconds = 10)
+    @SecurityRequirement(name = "bearerAuth")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    public ResponseEntity<?> applyLifeCyclePolicyAbortIncompleteMultipartUploads(@RequestBody AbortIncompleteMultipartDTO dto, HttpServletRequest request) {
+        this.unit.s3Service.createBucket(dto.getBucketName());
+        this.unit.s3Service.applyLifeCyclePolicyAbortIncompleteMultipartUploads(dto.getBucketName(), dto.getPrefix(), dto.getDays());
+
+        Map<String, Object> response = this.unit.responseDefault.response(
+                "Abort Incomplete Multipart Uploads Policy applied",
+                200,
+                request.getRequestURL().toString(),
+                "",
+                true
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PutMapping("/apply-life-cycle-policy-expire-noncurrent-versions")
+    @RateLimit(capacity = 15, refillTokens = 5, refillSeconds = 10)
+    @SecurityRequirement(name = "bearerAuth")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    public ResponseEntity<?> applyLifeCyclePolicyExpireNonCurrentVersions(@RequestBody ExpireNonCurrentVersionsDTO dto, HttpServletRequest request) {
+        this.unit.s3Service.createBucket(dto.getBucketName());
+        this.unit.s3Service.applyLifeCyclePolicyExpireNonCurrentVersions(dto.getBucketName(), dto.getPrefix(), dto.getNoncurrentDays());
+
+        Map<String, Object> response = this.unit.responseDefault.response(
+                "Expire NonCurrent Versions Policy applied to bucket " + dto.getBucketName(),
+                200,
+                request.getRequestURL().toString(),
+                "",
+                true
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PutMapping("/apply-life-cycle-policy-move-to-standardIA-and-expire-current-versions")
+    @RateLimit(capacity = 15, refillTokens = 5, refillSeconds = 10)
+    @SecurityRequirement(name = "bearerAuth")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    public ResponseEntity<?> applyLifeCyclePolicyMoveToStandardIAAndExpireCurrentVersions(@RequestBody MoveToStandardIAAndExpireCurrentVersionsDTO dto, HttpServletRequest request) {
+        this.unit.s3Service.createBucket(dto.getBucketName());
+        this.unit.s3Service.applyLifeCyclePolicyMoveToStandardIAAndExpireCurrentVersions(dto.getBucketName(), dto.getPrefix(), dto.getDays(), dto.getExpDays());
+
+        Map<String, Object> response = this.unit.responseDefault.response(
+                "Move To StandardIA and ExpireCurrentVersions Policy applied to bucket " + dto.getBucketName(),
+                200,
+                request.getRequestURL().toString(),
+                "",
+                true
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 
     @GetMapping("get-all-user")
     @RateLimit(capacity = 10, refillTokens = 5, refillSeconds = 10)
